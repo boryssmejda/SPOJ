29fc8f575fc89ed8ce84a5980a3794c7a8d01a7f
borys <borys.smejda@gmail.com>
2019-08-15 16:31:42 +0200

    Finished with changing structure on number_converter

--- SimpleNumbersConversion/src/number_converter.cpp
+++ SimpleNumbersConversion/src/number_converter.cpp
@@ -1,52 +1,41 @@
 #include "number_converter.hpp"
 
-class NumberConverter {
-private:
-	std::array<std::string, 36> digits
-	{ "0", "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8", "9" , "A" , "B" , "C" , "D", "E", "F" , "G" , "H" , "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-	std::string m_numberToBeConverted;
-	std::string m_convertedNumber;
-	int m_convertFromBase, m_convertToBase;
+std::string NumberConverter::convertToTargetBase(unsigned long long t_base10Representation) {
 
-
-	std::string convertToTargetBase(unsigned long long t_base10Representation) {
-
-		if (t_base10Representation == 0) {
-			return "0";
-		}
-
-		std::string result("");
-		while (t_base10Representation > 0) {
-
-			int currentDigit = t_base10Representation % m_convertToBase;
-
-			std::string digitInTargetBase = digits[currentDigit];
-
-			result += digitInTargetBase;
-			t_base10Representation /= m_convertToBase;
-
-		}
-
-		std::reverse(result.begin(), result.end());
-		return result;
+	if (t_base10Representation == 0) {
+		return "0";
 	}
 
-public:
-	NumberConverter(int t_fromBase, int t_toBase, std::string t_number)
+	std::string result("");
+	while (t_base10Representation > 0) {
+
+	int currentDigit = t_base10Representation % m_convertToBase;
+
+		std::string digitInTargetBase = digits[currentDigit];
+
+		result += digitInTargetBase;
+		t_base10Representation /= m_convertToBase;
+
+	}
+
+	std::reverse(result.begin(), result.end());
+	return result;
+}
+
+NumberConverter::NumberConverter(int t_fromBase, int t_toBase, std::string t_number)
 		:m_convertFromBase(t_fromBase), m_convertToBase(t_toBase),
 		m_numberToBeConverted(t_number)
-	{
-		m_convertedNumber = "";
-	}
+{
+	m_convertedNumber = "";
+}
 
-	std::string convertNumber() {
+std::string NumberConverter::convertNumber() {
 
-		unsigned long long base10Representation = std::stoull(m_numberToBeConverted, nullptr, m_convertFromBase);
+	unsigned long long base10Representation = std::stoull(m_numberToBeConverted, nullptr, m_convertFromBase);
 
-		m_convertedNumber = convertToTargetBase(base10Representation);
+	m_convertedNumber = convertToTargetBase(base10Representation);
 
-		return m_convertedNumber;
-	}
+	return m_convertedNumber;
+}
 
-};
 
