a9fb824c00d6d38bc0ada3a3422adee894caa8d2
borys <borys.smejda@gmail.com>
2019-08-15 16:33:56 +0200

    Finished with class definition

--- SimpleNumbersConversion/include/number_converter.hpp
+++ SimpleNumbersConversion/include/number_converter.hpp
@@ -1,4 +1,26 @@
 #ifndef  NUMBER_CONVERTER
 #define  NUMBER_CONVERTER
 
+#include <vector>
+#include <iostream>
+#include <string>
+#include <array>
+#include <algorithm>
+
+class NumberConverter {
+private:
+	std::array<std::string, 36> digits
+	{ "0", "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8", "9" , "A" , "B" , "C" , "D", "E", "F" , "G" , "H" , "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+	std::string m_numberToBeConverted;
+	std::string m_convertedNumber;
+	int m_convertFromBase, m_convertToBase;
+
+
+	std::string convertToTargetBase(unsigned long long t_base10Representation);
+
+public:
+	NumberConverter(int t_fromBase, int t_toBase, std::string t_number);
+	std::string convertNumber();
+};
+
 #endif // ! NUMBER_CONVERTER
